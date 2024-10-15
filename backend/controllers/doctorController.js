d44eb580626ff50b2cf8aa1a7b667b7b06eec512
AbrahamLincoln <s1136731@student.hsleiden.nl>
2024-10-15 16:07:49 +0200

    Add changes needed to use login and registration with the backend

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -4,11 +4,11 @@ import doctorModel from "../models/doctorModel.js"
 
 const changeAvailability = async (req, res) => {
     try {
-        const {docId} = req.body
+        const { docId } = req.body
 
         const docData = await doctorModel.findById(docId)
-        await doctorModel.findByIdAndUpdate(docId, {available: !docData.available})
-        res.json({success:true, message:'Availability Changed'})
+        await doctorModel.findByIdAndUpdate(docId, { available: !docData.available })
+        res.json({ success: true, message: 'Availability Changed' })
 
     } catch (error) {
         console.log(error)
@@ -16,13 +16,14 @@ const changeAvailability = async (req, res) => {
     }
 }
 
-const doctorList = async (req,res) =>{
+const doctorList = async (req, res) => {
     try {
         const doctors = await doctorModel.find({}).select(['-password', '-email'])
 
-        res.json({success:true, doctors})
+        res.json({ success: true, doctors })
     } catch (error) {
-        
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
 
